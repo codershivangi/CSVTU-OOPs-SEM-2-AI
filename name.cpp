0fd6ae8f5266db3f741e6228ac671b9cdfe0ec32
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-04-06 10:49:11 -0700

    Add files via upload

---
+++ name.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    cout << "enter the name:";
+    cin >> name;
+
+
+    cout << "entered name is : "<< name;
+    return 0;
+}
