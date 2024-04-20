57aa59e2980d589d1e82f505ce7b951213afc589
Shivangi <shivangi639661@gmail.com>
2024-04-20 08:51:21 -0700

    added

---
+++ simple_interest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float P = 1,R = 1,T = 1;
+
+    float SI = (P*R*T)/100;
+
+    cout << "Simple Interest = " << SI;
+
+    return 0;
+    
+}
