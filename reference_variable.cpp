70728f1bb54897b9928cd1026f649ec57fee5fd1
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-05-20 23:37:24 +0530

    Add files via upload

---
+++ reference_variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x =10;
+    int& ref = x;
+    
+    cout << "x =" << x << endl;
+    cout << "ref =" << ref << endl;
+
+    ref = 20;
+    cout << "x =" << x << endl;
+    cout << "ref =" << ref << endl;
+
+    return 0;
+    
+}
