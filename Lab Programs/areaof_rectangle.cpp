b6fb37e8d84c90338b7f2e421d28b08cc3b96400
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-05-25 20:44:39 +0530

    Add files via upload

---
+++ Lab Programs/areaof_rectangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int lenght,breadth,area;
+
+    cout <<"Enter the lenght of the rectangle:";
+    cin >> lenght;
+    cout << "Enter the breadth of rectangle:";
+    cin >> breadth;
+
+    area = lenght *breadth;
+
+    cout << "Area of rectangle:"<< area;
+
+    return 0;
+}
