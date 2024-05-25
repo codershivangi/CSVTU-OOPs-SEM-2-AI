76f4b0d015e5916d41b6999442c2d00307a3a91b
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-05-25 20:14:27 +0530

    Add files via upload

---
+++ Lab Programs/name.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout << "enter the name:";
+    cin >> name;
+
+    cout << "entered name is:"<< name;
+    return 0;
+    
+}
