e3c3fca755d2b71296ec8b0da89ddfd20f584103
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-05-25 20:46:24 +0530

    Delete name_getline.cpp

--- name_getline.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    string name; 
-    cout << "enter the name:";
-    getline(cin,name,'@');
-    cout << "entered name is:"<< name;
-    return 0;
-}
