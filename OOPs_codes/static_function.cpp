f11f7ffca448fe966d2029ff70c66f8443137dbc
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-08-09 23:41:53 -0700

    Add files via upload

--- OOPs_codes/static_function.cpp
+++ OOPs_codes/static_function.cpp
@@ -1,30 +1,23 @@
 #include<iostream>
 using namespace std;
-class box
-{
-    private:
-    static int length;
-    static int breadth;
-    static int height;
-    public:
-    static void print()
-    {
-        cout << "The value of the length is:" << length << endl;
-        cout << "The value of the breadth is:" << breadth << endl;
-        cout << "The value of the height is:" << height << endl;
-    }
+class Student {
+public:
+      static int total;
+      Student(){
+        total += 1;
+      }
 };
-
-int box::length=10;
-int box::breadth=20;
-int box::height=30;
+ 
+int Student::total = 0;
 int main()
 {
-    box b;
-    cout << "Static member function is called through object name:\n" << endl;
-    b.print();
-    cout << "\n Static member function is called through class name:\n" << endl;
-    box::print();
+    
+    Student s1;
+    cout << "Number of students:" << s1.total << endl;
+    Student s2;
+    cout << "Number of students:" << s2.total << endl;
+    Student s3;
+    cout << "Number of students:" << s3.total << endl;
     return 0;
 
 }
