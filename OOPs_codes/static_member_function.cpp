ebaf1ea242935d7365af9b7b67f686a05f1c9d8d
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-08-11 21:55:06 -0700

    Add files via upload

---
+++ OOPs_codes/static_member_function.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Box
+{
+private:
+static int length;
+static int breadth;
+static int height;
+public:
+static void print()
+{
+cout << "The value of the length is:" << length << endl;
+cout << "The value of the breadth is:" << breadth << endl;
+cout << "The value of the heigth is:" << height << endl;
+}
+};
+int Box::length = 10;
+int Box::breadth = 20;
+int Box::height = 30;
+int main()
+{
+    Box b;
+    cout << "Static member function is called through object name:\n"<< endl;
+    b.print();
+    cout << "\nStatic member function is called through class name:\n"<< endl;
+    Box::print();
+    return 0;
+}
